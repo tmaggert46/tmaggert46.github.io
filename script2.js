8af62395ff9a4f52b68d01a852056d8c0700c49d
tmaggert46 <134242928+tmaggert46@users.noreply.github.com>
2024-07-28 19:11:49 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,568 @@
+const characterData = {
+    "Alear": [
+        { name: "HP", value: 60 },
+        { name: "Str", value: 35 },
+        { name: "Mag", value: 20 },
+        { name: "Dex", value: 45 },
+        { name: "Spd", value: 50 },
+        { name: "Def", value: 40 },
+        { name: "Res", value: 25 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 5 }
+    ],
+    "Vander": [
+        { name: "HP", value: 60 },
+        { name: "Str", value: 25 },
+        { name: "Mag", value: 10 },
+        { name: "Dex", value: 25 },
+        { name: "Spd", value: 35 },
+        { name: "Def", value: 35 },
+        { name: "Res", value: 20 },
+        { name: "Lck", value: 10 },
+        { name: "Bld", value: 5 }
+    ],
+    "Clanne": [
+        { name: "HP", value: 40 },
+        { name: "Str", value: 35 },
+        { name: "Mag", value: 10 },
+        { name: "Dex", value: 40 },
+        { name: "Spd", value: 50 },
+        { name: "Def", value: 30 },
+        { name: "Res", value: 25 },
+        { name: "Lck", value: 20 },
+        { name: "Bld", value: 5 }
+    ],
+    "Framme": [
+        { name: "HP", value: 55 },
+        { name: "Str", value: 30 },
+        { name: "Mag", value: 25 },
+        { name: "Dex", value: 35 },
+        { name: "Spd", value: 55 },
+        { name: "Def", value: 25 },
+        { name: "Res", value: 30 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 0 }
+    ],
+    "Alfred": [
+        { name: "HP", value: 65 },
+        { name: "Str", value: 40 },
+        { name: "Mag", value: 5 },
+        { name: "Dex", value: 35 },
+        { name: "Spd", value: 40 },
+        { name: "Def", value: 40 },
+        { name: "Res", value: 20 },
+        { name: "Lck", value: 40 },
+        { name: "Bld", value: 10 }
+    ],
+    "Etie": [
+        { name: "HP", value: 45 },
+        { name: "Str", value: 40 },
+        { name: "Mag", value: 0 },
+        { name: "Dex", value: 25 },
+        { name: "Spd", value: 35 },
+        { name: "Def", value: 25 },
+        { name: "Res", value: 30 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 5 }
+    ],
+    "Boucheron": [
+        { name: "HP", value: 85 },
+        { name: "Str", value: 20 },
+        { name: "Mag", value: 0 },
+        { name: "Dex", value: 50 },
+        { name: "Spd", value: 45 },
+        { name: "Def", value: 35 },
+        { name: "Res", value: 20 },
+        { name: "Lck", value: 15 },
+        { name: "Bld", value: 20 }
+    ],
+    "Céline": [
+        { name: "HP", value: 50 },
+        { name: "Str", value: 35 },
+        { name: "Mag", value: 25 },
+        { name: "Dex", value: 30 },
+        { name: "Spd", value: 45 },
+        { name: "Def", value: 30 },
+        { name: "Res", value: 40 },
+        { name: "Lck", value: 50 },
+        { name: "Bld", value: 5 }
+    ],
+    "Chloé": [
+        { name: "HP", value: 75 },
+        { name: "Str", value: 25 },
+        { name: "Mag", value: 35 },
+        { name: "Dex", value: 40 },
+        { name: "Spd", value: 55 },
+        { name: "Def", value: 30 },
+        { name: "Res", value: 25 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 5 }
+    ],
+    "Louis": [
+        { name: "HP", value: 75 },
+        { name: "Str", value: 40 },
+        { name: "Mag", value: 0 },
+        { name: "Dex", value: 25 },
+        { name: "Spd", value: 25 },
+        { name: "Def", value: 50 },
+        { name: "Res", value: 20 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 15 }
+    ],
+    "Yunaka": [
+        { name: "HP", value: 50 },
+        { name: "Str", value: 35 },
+        { name: "Mag", value: 25 },
+        { name: "Dex", value: 40 },
+        { name: "Spd", value: 45 },
+        { name: "Def", value: 15 },
+        { name: "Res", value: 45 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 5 }
+    ],
+    "Alcryst": [
+        { name: "HP", value: 65 },
+        { name: "Str", value: 30 },
+        { name: "Mag", value: 10 },
+        { name: "Dex", value: 40 },
+        { name: "Spd", value: 45 },
+        { name: "Def", value: 30 },
+        { name: "Res", value: 20 },
+        { name: "Lck", value: 15 },
+        { name: "Bld", value: 10 }
+    ],
+    "Citrinne": [
+        { name: "HP", value: 45 },
+        { name: "Str", value: 10 },
+        { name: "Mag", value: 40 },
+        { name: "Dex", value: 25 },
+        { name: "Spd", value: 30 },
+        { name: "Def", value: 20 },
+        { name: "Res", value: 40 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 5 }
+    ],
+    "Lapis": [
+        { name: "HP", value: 55 },
+        { name: "Str", value: 25 },
+        { name: "Mag", value: 20 },
+        { name: "Dex", value: 35 },
+        { name: "Spd", value: 55 },
+        { name: "Def", value: 35 },
+        { name: "Res", value: 30 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 5 }
+    ],
+    "Diamant": [
+        { name: "HP", value: 75 },
+        { name: "Str", value: 30 },
+        { name: "Mag", value: 15 },
+        { name: "Dex", value: 20 },
+        { name: "Spd", value: 40 },
+        { name: "Def", value: 40 },
+        { name: "Res", value: 25 },
+        { name: "Lck", value: 20 },
+        { name: "Bld", value: 15 }
+    ],
+    "Amber": [
+        { name: "HP", value: 65 },
+        { name: "Str", value: 45 },
+        { name: "Mag", value: 0 },
+        { name: "Dex", value: 25 },
+        { name: "Spd", value: 30 },
+        { name: "Def", value: 35 },
+        { name: "Res", value: 5 },
+        { name: "Lck", value: 35 },
+        { name: "Bld", value: 15 }
+    ],
+    "Jade": [
+        { name: "HP", value: 55 },
+        { name: "Str", value: 35 },
+        { name: "Mag", value: 25 },
+        { name: "Dex", value: 35 },
+        { name: "Spd", value: 30 },
+        { name: "Def", value: 40 },
+        { name: "Res", value: 30 },
+        { name: "Lck", value: 20 },
+        { name: "Bld", value: 10 }
+    ],
+    "Ivy": [
+        { name: "HP", value: 55 },
+        { name: "Str", value: 25 },
+        { name: "Mag", value: 30 },
+        { name: "Dex", value: 25 },
+        { name: "Spd", value: 40 },
+        { name: "Def", value: 30 },
+        { name: "Res", value: 35 },
+        { name: "Lck", value: 15 },
+        { name: "Bld", value: 10 }
+    ],
+    "Kagetsu": [
+        { name: "HP", value: 60 },
+        { name: "Str", value: 30 },
+        { name: "Mag", value: 15 },
+        { name: "Dex", value: 50 },
+        { name: "Spd", value: 50 },
+        { name: "Def", value: 40 },
+        { name: "Res", value: 25 },
+        { name: "Lck", value: 40 },
+        { name: "Bld", value: 10 }
+    ],
+    "Zelkov": [
+        { name: "HP", value: 65 },
+        { name: "Str", value: 35 },
+        { name: "Mag", value: 15 },
+        { name: "Dex", value: 40 },
+        { name: "Spd", value: 35 },
+        { name: "Def", value: 35 },
+        { name: "Res", value: 15 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 10 }
+    ],
+    "Fogado": [
+        { name: "HP", value: 60 },
+        { name: "Str", value: 30 },
+        { name: "Mag", value: 25 },
+        { name: "Dex", value: 30 },
+        { name: "Spd", value: 55 },
+        { name: "Def", value: 30 },
+        { name: "Res", value: 35 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 10 }
+    ],
+    "Pandreo": [
+        { name: "HP", value: 60 },
+        { name: "Str", value: 5 },
+        { name: "Mag", value: 30 },
+        { name: "Dex", value: 45 },
+        { name: "Spd", value: 45 },
+        { name: "Def", value: 15 },
+        { name: "Res", value: 55 },
+        { name: "Lck", value: 30 },
+        { name: "Bld", value: 15 }
+    ],
+    "Bunet": [
+        { name: "HP", value: 65 },
+        { name: "Str", value: 30 },
+        { name: "Mag", value: 10 },
+        { name: "Dex", value: 40 },
+        { name: "Spd", value: 35 },
+        { name: "Def", value: 45 },
+        { name: "Res", value: 25 },
+        { name: "Lck", value: 40 },
+        { name: "Bld", value: 10 }
+    ],
+    "Timerra": [
+        { name: "HP", value: 55 },
+        { name: "Str", value: 25 },
+        { name: "Mag", value: 25 },
+        { name: "Dex", value: 45 },
+        { name: "Spd", value: 45 },
+        { name: "Def", value: 30 },
+        { name: "Res", value: 30 },
+        { name: "Lck", value: 30 },
+        { name: "Bld", value: 10 }
+    ],
+    "Panette": [
+        { name: "HP", value: 75 },
+        { name: "Str", value: 45 },
+        { name: "Mag", value: 10 },
+        { name: "Dex", value: 40 },
+        { name: "Spd", value: 25 },
+        { name: "Def", value: 30 },
+        { name: "Res", value: 15 },
+        { name: "Lck", value: 20 },
+        { name: "Bld", value: 15 }
+    ],
+    "Merrin": [
+        { name: "HP", value: 55 },
+        { name: "Str", value: 25 },
+        { name: "Mag", value: 25 },
+        { name: "Dex", value: 40 },
+        { name: "Spd", value: 50 },
+        { name: "Def", value: 30 },
+        { name: "Res", value: 30 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 10 }
+    ],
+    "Hortensia": [
+        { name: "HP", value: 40 },
+        { name: "Str", value: 20 },
+        { name: "Mag", value: 20 },
+        { name: "Dex", value: 35 },
+        { name: "Spd", value: 50 },
+        { name: "Def", value: 25 },
+        { name: "Res", value: 55 },
+        { name: "Lck", value: 50 },
+        { name: "Bld", value: 0 }
+    ],
+    "Seadall": [
+        { name: "HP", value: 55 },
+        { name: "Str", value: 25 },
+        { name: "Mag", value: 15 },
+        { name: "Dex", value: 25 },
+        { name: "Spd", value: 50 },
+        { name: "Def", value: 25 },
+        { name: "Res", value: 25 },
+        { name: "Lck", value: 35 },
+        { name: "Bld", value: 10 }
+    ],
+    "Rosado": [
+        { name: "HP", value: 75 },
+        { name: "Str", value: 45 },
+        { name: "Mag", value: 25 },
+        { name: "Dex", value: 40 },
+        { name: "Spd", value: 45 },
+        { name: "Def", value: 30 },
+        { name: "Res", value: 30 },
+        { name: "Lck", value: 20 },
+        { name: "Bld", value: 5 }
+    ],
+    "Goldmary": [
+        { name: "HP", value: 65 },
+        { name: "Str", value: 30 },
+        { name: "Mag", value: 5 },
+        { name: "Dex", value: 25 },
+        { name: "Spd", value: 25 },
+        { name: "Def", value: 55 },
+        { name: "Res", value: 25 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 5 }
+    ],
+    "Lindon": [
+        { name: "HP", value: 65 },
+        { name: "Str", value: 25 },
+        { name: "Mag", value: 25 },
+        { name: "Dex", value: 25 },
+        { name: "Spd", value: 40 },
+        { name: "Def", value: 25 },
+        { name: "Res", value: 40 },
+        { name: "Lck", value: 15 },
+        { name: "Bld", value: 10 }
+    ],
+    "Saphir": [
+        { name: "HP", value: 80 },
+        { name: "Str", value: 35 },
+        { name: "Mag", value: 0 },
+        { name: "Dex", value: 25 },
+        { name: "Spd", value: 30 },
+        { name: "Def", value: 30 },
+        { name: "Res", value: 5 },
+        { name: "Lck", value: 20 },
+        { name: "Bld", value: 10 }
+    ],
+    "Veyle": [
+        { name: "HP", value: 40 },
+        { name: "Str", value: 25 },
+        { name: "Mag", value: 45 },
+        { name: "Dex", value: 35 },
+        { name: "Spd", value: 30 },
+        { name: "Def", value: 25 },
+        { name: "Res", value: 35 },
+        { name: "Lck", value: 20 },
+        { name: "Bld", value: 0 }
+    ],
+    "Mauvier": [
+        { name: "HP", value: 70 },
+        { name: "Str", value: 35 },
+        { name: "Mag", value: 40 },
+        { name: "Dex", value: 40 },
+        { name: "Spd", value: 35 },
+        { name: "Def", value: 50 },
+        { name: "Res", value: 45 },
+        { name: "Lck", value: 15 },
+        { name: "Bld", value: 15 }
+    ],
+    "Anna": [
+        { name: "HP", value: 55 },
+        { name: "Str", value: 15 },
+        { name: "Mag", value: 50 },
+        { name: "Dex", value: 50 },
+        { name: "Spd", value: 50 },
+        { name: "Def", value: 20 },
+        { name: "Res", value: 35 },
+        { name: "Lck", value: 45 },
+        { name: "Bld", value: 5 }
+    ],
+    "Jean": [
+        { name: "HP", value: 50 },
+        { name: "Str", value: 20 },
+        { name: "Mag", value: 20 },
+        { name: "Dex", value: 35 },
+        { name: "Spd", value: 40 },
+        { name: "Def", value: 25 },
+        { name: "Res", value: 20 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 5 }
+    ],
+    "Nel": [
+        { name: "HP", value: 55 },
+        { name: "Str", value: 30 },
+        { name: "Mag", value: 25 },
+        { name: "Dex", value: 40 },
+        { name: "Spd", value: 45 },
+        { name: "Def", value: 30 },
+        { name: "Res", value: 35 },
+        { name: "Lck", value: 30 },
+        { name: "Bld", value: 10 }
+    ],
+    "Rafal": [
+        { name: "HP", value: 70 },
+        { name: "Str", value: 40 },
+        { name: "Mag", value: 10 },
+        { name: "Dex", value: 40 },
+        { name: "Spd", value: 35 },
+        { name: "Def", value: 45 },
+        { name: "Res", value: 20 },
+        { name: "Lck", value: 10 },
+        { name: "Bld", value: 10 }
+    ],
+    "Zelestia": [
+        { name: "HP", value: 60 },
+        { name: "Str", value: 35 },
+        { name: "Mag", value: 35 },
+        { name: "Dex", value: 35 },
+        { name: "Spd", value: 50 },
+        { name: "Def", value: 30 },
+        { name: "Res", value: 35 },
+        { name: "Lck", value: 20 },
+        { name: "Bld", value: 5 }
+    ],
+    "Gregory": [
+        { name: "HP", value: 55 },
+        { name: "Str", value: 30 },
+        { name: "Mag", value: 50 },
+        { name: "Dex", value: 20 },
+        { name: "Spd", value: 25 },
+        { name: "Def", value: 35 },
+        { name: "Res", value: 50 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 5 }
+    ],
+    "Madeline": [
+        { name: "HP", value: 75 },
+        { name: "Str", value: 50 },
+        { name: "Mag", value: 20 },
+        { name: "Dex", value: 15 },
+        { name: "Spd", value: 15 },
+        { name: "Def", value: 55 },
+        { name: "Res", value: 25 },
+        { name: "Lck", value: 25 },
+        { name: "Bld", value: 10 }
+    ]
+};
+
+const statColors = {
+    HP: "#004d00",        // Dark Green
+    Str: "#8b0000",       // Dark Red
+    Mag: "#00008b",       // Dark Blue
+    Dex: "#800080",       // Purple
+    Spd: "#ff00ff",       // Magenta
+    Def: "#000000",       // Black
+    Res: "#00ffff",       // Aqua
+    Lck: "#b8860b",       // Dark Yellow
+    Bld: "#808080"        // Gray
+};
+
+const margin = { top: 20, right: 30, bottom: 40, left: 40 };
+const width = 800 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+const svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+const x = d3.scaleBand()
+    .range([0, width])
+    .padding(0.1);
+
+const y = d3.scaleLinear()
+    .range([height, 0]);
+
+svg.append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0,${height})`);
+
+svg.append("g")
+    .attr("class", "y-axis");
+
+function updateChart(selectedCharacter) {
+    const data = characterData[selectedCharacter];
+
+    x.domain(data.map(d => d.name));
+    y.domain([0, 100]).nice();
+
+    svg.selectAll(".bar").remove();
+    svg.selectAll(".bar-label").remove();
+
+    svg.selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", d => x(d.name))
+        .attr("y", height)
+        .attr("width", x.bandwidth())
+        .attr("height", 0)
+        .attr("fill", d => statColors[d.name])
+        .transition()
+        .delay(250)
+        .duration(1000)
+        .attr("y", d => y(d.value))
+        .attr("height", d => height - y(d.value));
+
+    svg.select(".x-axis")
+        .call(d3.axisBottom(x));
+
+    svg.select(".y-axis")
+        .call(d3.axisLeft(y));
+
+    // Add text elements to display bar values
+    svg.selectAll(".bar-label")
+        .data(data)
+        .enter()
+        .append("text")
+        .attr("class", "bar-label")
+        .attr("x", d => x(d.name) + x.bandwidth() / 2)
+        .attr("y", height)
+        .attr("text-anchor", "middle")
+        .merge(svg.selectAll(".bar-label"))
+        .transition()
+        .delay(250)
+        .duration(1000)
+        .attr("x", d => x(d.name) + x.bandwidth() / 2)
+        .attr("y", d => y(d.value) - 5)
+        .text(d => d.value.toFixed(0));
+}
+
+function initializeDropdowns() {
+    const classSelect = d3.select("#class-select");
+
+    // List of classes to highlight
+    const highlightedClasses = ["Etie", "Chloé", "Veyle"];
+
+    // Add class options
+    Object.keys(characterData).forEach(cls => {
+        const option = classSelect.append("option")
+            .attr("value", cls)
+            .text(cls);
+        
+        // Highlight specified classes
+        if (highlightedClasses.includes(cls)) {
+            option.attr("class", "highlighted-option");
+        }
+    });
+} 
+
+updateChart("Alear");
+initializeDropdowns();
+
+// Add event listener to dropdown
+d3.select("#class-select").on("change", function() {
+    const selectedCharacter = d3.select(this).property("value");
+    updateChart(selectedCharacter);
+});
 
